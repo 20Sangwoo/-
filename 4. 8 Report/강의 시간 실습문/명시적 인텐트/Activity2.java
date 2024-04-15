35e0a15d4068ac828e27016303ac8a2f325b4d75
20Sangwoo <144245121+20Sangwoo@users.noreply.github.com>
2024-04-15 12:24:46 +0900

    Add files via upload
    Signed-off-by: 20Sangwoo <144245121+20Sangwoo@users.noreply.github.com>

---
+++ 4. 8 Report/강의 시간 실습문/명시적 인텐트/Activity2.java
@@ -0,0 +1,24 @@
+package com.example.activity1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout2);
+
+        Button b = findViewById(R.id.Button01);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // 현재 액티비티 종료
+            }
+        });
+    }
+}
